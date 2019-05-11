1bcf1bdb6d20ceb4a72906236596174a3fc0a5b0
kukus888 <jskora696@gmail.com>
2019-05-11 19:24:34 +0200

    Base commit

---
+++ posledniZvoneni/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Timers;
+using System.Diagnostics;
+
+namespace posledniZvoneni
+{
+    public partial class Form1 : Form
+    {
+        public Form1(Bitmap pozadi,Bitmap bsod)
+        {
+            Cursor.Hide();
+            this.BackgroundImage = pozadi;
+            this.Width = Screen.PrimaryScreen.Bounds.Width;
+            this.Height = Screen.PrimaryScreen.Bounds.Height;
+            this.Visible = true;
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            this.Show();
+            Thread.Sleep(5000);
+            this.BackgroundImage = bsod;
+            this.Refresh();
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ posledniZvoneni/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+using CSCore.Codecs.WAV;
+using System.Threading;
+using CSCore.Streams;
+using System.Diagnostics;
+
+namespace posledniZvoneni
+{
+    static class Program
+    {
+        /// <summary>
+        /// Hlavní vstupní bod aplikace.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var zvuk = new System.Media.SoundPlayer(Properties.Resources._50HzSquare);
+            //Create a new bitmap.
+            var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            // Create a graphics object from the bitmap.
+            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+            // Take the screenshot from the upper left corner to the right bottom corner.
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            // Magie, máš screenshot v bmpScreenshot
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Cursor.Hide();
+            new Thread(new ThreadStart(() => Frm(bmpScreenshot))).Start();
+            zvuk.Play();
+        }
+        public static void Frm(Bitmap bmpScreenshot) {
+            Application.Run(new Form1(bmpScreenshot,Properties.Resources.bsod));
+        }
+    }
+}
